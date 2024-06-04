e2bcbfe3628001405599402e265e5a4b8db88d1d
Rodny Estrada <rrodnyestrada1@gmail.com>
2024-06-04 10:17:25 -0400

    fix: fixed QR generation

--- src/ui/qr/index.js
+++ src/ui/qr/index.js
@@ -4,7 +4,7 @@ import { Scanner as QRScannerLib } from '@yudiel/react-qr-scanner'
 
 export const QRCode = ({className, value}) => {
   return (
-    <div className='p-5 w-64 rounded-xl bg-current'>
+    <div className='p-5 w-64 rounded-xl bg-white'>
       <QRCodeLib 
         className='w-full h-auto' 
         value={value}
